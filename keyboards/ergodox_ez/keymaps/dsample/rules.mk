7d77203001a92fa6eea927c77eb87302a344738c
Duncan Sample <code@antimatters.co.uk>
2019-04-01 22:38:01 +0100

    ✨ Adding TX Bolt layer

--- keyboards/ergodox_ez/keymaps/dsample/rules.mk
+++ keyboards/ergodox_ez/keymaps/dsample/rules.mk
@@ -2,3 +2,5 @@
 # See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
 COMMAND_ENABLE = no
 MOUSEKEY_ENABLE = no
+
+STENO_ENABLE = yes          # Additional protocols for Stenography(+1700), requires VIRTSER
